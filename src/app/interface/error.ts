45313d539ac78ef316e2fc2c0fa64f3a289c4dd3
Md Ibrahim <mdrubel072125@gmail.com>
2023-12-20 16:26:19 +0600

    'zod-error-handle-own-my-style'

---
+++ src/app/interface/error.ts
@@ -0,0 +1,7 @@
+export type TErrorSouce = {
+    path: string | number | (string | number)[];
+    message: string;
+  }[];
+
+  export type TErorDetails = [] | null | number |string
+  export type TErrorMessage = (string | number)[][]
--- src/app/middlwares/globalErrorhandler.ts
+++ src/app/middlwares/globalErrorhandler.ts
@@ -1,16 +1,63 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from 'express';
+import httpStatus from 'http-status';
+import { ZodError } from 'zod';
+import { TErrorSouce } from '../interface/error';
+import config from '../config';
 
-const golobalErrorHandler = (err : any, req: Request, res: Response, next: NextFunction)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Something went wrong'
+const golobalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  //setting default values
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong';
+
+  let errorSources: TErrorSouce = [
+    {
+      path: '',
+      message: '',
+    },
+  ];
+
+  //zod handler function
+  const zodErrorHandler = (err: ZodError) => {
+    const errorSources : TErrorSouce= err.issues.map((issue) => {
+      return {
+        path: issue?.path[issue?.path.length - 1],
+        message: issue?.message,
+      };
+    });
+    return {
+      statusCode: httpStatus.BAD_REQUEST,
+      message: 'Validation Error',
+      errorSources,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = zodErrorHandler(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err
-  })
-}
+    errorSources,
+    stack: config.NODE_ENV === 'development'? err?.stack : null
+  });
+};
 
 export default golobalErrorHandler;
+
+//error pattern
+/*
+1.success
+2.message
+3.errorSources:[
+  path: "",
+  message: ""
+]
+stack
+*/

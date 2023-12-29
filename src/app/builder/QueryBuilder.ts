e3cab8c5ebfa41fae83e38afe53a2ae511981774
Md Ibrahim <mdrubel072125@gmail.com>
2023-12-29 23:59:27 +0600

    course

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -59,9 +59,21 @@ class QueryBuilder<T> {
   fields() {
     const fields =
       (this?.query?.fields as string)?.split(',')?.join(' ') || '-__v';
-
     this.modelQuery = this.modelQuery.select(fields);
     return this;
+  }
+  async countTotal() {
+    const totalQueries = this.modelQuery.getFilter();
+    const total = await this.modelQuery.model.countDocuments(totalQueries);
+    const page = Number(this?.query?.page) || 1;
+    const limit = Number(this?.query?.limit) || 10;
+    const totalPage = Math.ceil(total / limit);
+    return {
+      page,
+      limit,
+      total,
+      totalPage,
+    };
   }
 }
 
